43d79cb96dcff17dee782490567fea177768eb75
Roin Zhang <roin.zhang@skyscanner.net>
2019-07-31 18:53:42 +0800

    增加kafka的简易的生产消费者

--- kafka/src/main/java/cn/xiaoyu/learning/kafka/simple/SimpleConsumer.java
+++ kafka/src/main/java/cn/xiaoyu/learning/kafka/simple/SimpleConsumer.java
@@ -1,11 +1,12 @@
 package cn.xiaoyu.learning.kafka.simple;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
 
+import java.time.Duration;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -16,13 +17,13 @@ public class SimpleConsumer {
         String topicName = "topic.test";
 
         Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "master:9091,master:9092,master:9093");
         // 消费者组 同名的消费者组采用队列模型，不同名的消费者组采用发布订阅模型
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
         // 最大心跳停止时间，超过则认为机器故障
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
         // 自动提交偏移量 设置enable.auto.commit，偏移量有auto.commit.interval.ms控制自动提交的频率
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
                 "org.apache.kafka.common.serialization.StringDeserializer");
@@ -31,15 +32,40 @@ public class SimpleConsumer {
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
         consumer.subscribe(Arrays.asList(topicName));
+        // 正则表达式匹配多个topic
+//        consumer.subscribe(Pattern.compile("topic.*"));
 
         System.out.println("Subscribed to topic " + topicName);
 
+        Map<TopicPartition, OffsetAndMetadata> currentOffsets = new HashMap<>();
         while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(100);
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(2));
             for (ConsumerRecord<String, String> record : records) {
                 System.out.printf("offset = %d, key = %s, value = %s \n",
                         record.offset(), record.key(), record.value());
+
+                // 提交指定offset
+                currentOffsets.put(new TopicPartition(record.topic(), record.partition()),
+                        new OffsetAndMetadata(record.offset() + 1, "no metadata"));
+                if (record.offset() % 8 == 0) {
+                    consumer.commitSync(currentOffsets, Duration.ofSeconds(1));
+                }
+
             }
+
+//            try {
+//                // 手动异步提交偏移量 提交此次poll返回的最大offset
+//                consumer.commitAsync((offsets, ex) -> {
+//                    if (ex != null) {
+//                        System.out.printf("Commit failed for offsets %s", offsets);
+//                    }
+//                });
+//            } catch (CommitFailedException ex) {
+//                ex.printStackTrace();
+//            } finally {
+//                // 同步提交
+//                consumer.commitSync();
+//            }
         }
     }
 }
--- kafka/src/main/java/cn/xiaoyu/learning/kafka/simple/SimpleProducer.java
+++ kafka/src/main/java/cn/xiaoyu/learning/kafka/simple/SimpleProducer.java
@@ -1,19 +1,21 @@
 package cn.xiaoyu.learning.kafka.simple;
 
-import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
 
 /**
  * 简易生产者
  */
 public class SimpleProducer {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) {
         String topicName = "topic.test";
 
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "master:9091,master:9092,master:9093");
         // 判断请求是否成功发送
         props.put(ProducerConfig.ACKS_CONFIG, "all");
         // 请求失败，生产者重试次数。启动重试，会有重复消息的可能性
@@ -32,13 +34,14 @@ public class SimpleProducer {
         Producer<String, String> producer = new KafkaProducer<>(props);
 
         for (int i = 0; i < 10; i++) {
+            // 创建发送消息记录
             ProducerRecord<String, String> record = new ProducerRecord<>(topicName,
                     Integer.toString(i), Integer.toString(i));
-            // send是异步的，添加消息到缓冲区等待发送，并立即返回
+            // send是异步的，添加消息到缓冲区等待发送，并立即返回 Fire-and-forget
 //            producer.send(record);
-            // 阻塞调用
+            // 阻塞调用 Synchronous send
 //            RecordMetadata metadata = producer.send(record).get();
-            // 无阻塞回调，利用回调参数来接受回调通知
+            // 无阻塞回调，利用回调参数来接受回调通知 Asynchronous send
             producer.send(record, (metadata, e) -> {
                 // 一般会在生产者的IO线程中执行，对于有阻塞或者耗时的回调，可以交给线程池处理
                 if (e != null) {
