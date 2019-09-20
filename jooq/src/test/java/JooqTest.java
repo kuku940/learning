dd185c9be6a4d03ec1ada614b4a79a4a2870177f
Roin Zhang <roin.zhang@skyscanner.net>
2019-09-20 13:54:26 +0800

    添加Jooq单元测试

---
+++ jooq/src/test/java/JooqTest.java
@@ -0,0 +1,77 @@
+import cn.xiaoyu.learning.data.jooq.Tables;
+import cn.xiaoyu.learning.data.jooq.tables.pojos.User;
+import cn.xiaoyu.learning.data.jooq.tables.records.UserRecord;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class JooqTest {
+    private DSLContext dslContext;
+
+    @Before
+    public void before() {
+        this.dslContext = getDSLContext();
+    }
+
+    @Test
+    public void insert() {
+        User user = new User();
+        user.setName("Jack");
+        user.setAge(21);
+        user.setEmail("jack@ali.com");
+        UserRecord userRecord = dslContext.newRecord(Tables.USER, user);
+        int i = userRecord.store();
+        Assert.assertEquals(1, i);
+    }
+
+    @Test
+    public void select() {
+        Result<Record> result = dslContext.select().from(Tables.USER).fetch();
+        result.get(0);
+    }
+
+    @Test
+    public void trans() {
+        try {
+            dslContext.transaction(configuration -> {
+                User user = new User(null, "xiaoming", 26, "xiaoming@sql.cn");
+                UserRecord userRecord = DSL.using(configuration).newRecord(Tables.USER, user);
+                Assert.assertEquals(1, userRecord.store());
+
+                throw new RuntimeException("mock an exception");
+            });
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Test
+    public void delete() {
+        dslContext.delete(Tables.USER)
+                .where(Tables.USER.NAME.eq("Jack"))
+                .execute();
+
+    }
+
+    private DSLContext getDSLContext() {
+        try {
+            Connection connection =
+                    DriverManager.getConnection("jdbc:mysql://localhost:3306/payment", "root", "12345678");
+            return DSL.using(connection, SQLDialect.MYSQL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
