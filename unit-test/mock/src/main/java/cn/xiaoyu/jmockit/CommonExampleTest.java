64e24d6c103d60fd150fcc9d546c20ab551f5a56
章小雨 <xiaoyu3@sfmail.sf-express.com>
2020-08-25 11:26:47 +0800

    Jmockit添加其他的单元测试

--- unit-test/mock/src/main/java/cn/xiaoyu/jmockit/CommonExampleTest.java
+++ unit-test/mock/src/main/java/cn/xiaoyu/jmockit/CommonExampleTest.java
@@ -1,11 +1,6 @@
 package cn.xiaoyu.jmockit;
 
-import mockit.Expectations;
-import mockit.Injectable;
-import mockit.Mock;
-import mockit.MockUp;
-import mockit.Tested;
-import mockit.Verifications;
+import mockit.*;
 import mockit.integration.junit4.JMockit;
 import org.junit.Assert;
 import org.junit.Before;
@@ -22,6 +17,8 @@ import static org.mockito.Matchers.anyLong;
  */
 @RunWith(JMockit.class)
 public class CommonExampleTest {
+    @Injectable
+    UserDao userDao;
     @Tested
     UserService userService;
 
@@ -30,20 +27,36 @@ public class CommonExampleTest {
     }
 
     @Test
-    public void testLogin(@Injectable UserDao userDao) {
+    public void testLogin() {
         User user = new User();
         new Expectations() {{
             userDao.get(withInstanceOf(Long.class));
             returns(user, null);
             times = 2;
+            // mock无返回值的
+            userDao.insert(withInstanceOf(User.class));
+            times = 1;
         }};
 
         Assert.assertTrue(userService.login(1L));
         Assert.assertFalse(userService.login(1L));
+
+        userService.regist(new User());
+        new Verifications() {{
+            userDao.insert((User) any);
+            times = 1;
+        }};
     }
 
     @Test
-    public void testInsert(@Injectable UserDao userDao) {
+    public void testGetValue() {
+        // 设置私有成员变量的值
+        Deencapsulation.setField(userService, "value", "5");
+        Assert.assertEquals("5", userService.getValue());
+    }
+
+    @Test
+    public void testInsert() {
         new MockUp<UserDao>(UserDao.class) {
             @Mock
             public void insert(User user) {
@@ -57,6 +70,19 @@ public class CommonExampleTest {
         }};
     }
 
+    @Test
+    public void testDoGetSession2() {
+        new MockUp<CommonService>(CommonService.class) {
+            @Mock
+            public String getSessionId() {
+                return "123456";
+            }
+        };
+
+        String session = userService.doGetSession();
+        Assert.assertEquals("123456", session);
+    }
+
 }
 
 class User {
@@ -64,17 +90,32 @@ class User {
 }
 
 interface UserDao {
-    User get(Long userId);
+    <T> User get(T userId);
 
     void insert(User user);
 }
 
-class UserService {
+class CommonService {
+    public String getSessionId() {
+        return "sessionId";
+    }
+}
+
+class UserService extends CommonService {
     private UserDao userDao;
+    private String value;
 
     public boolean login(Long userId) {
         User user = userDao.get(userId);
         return user != null;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public String doGetSession() {
+        return this.getSessionId();
     }
 
     public void regist(User user) {
