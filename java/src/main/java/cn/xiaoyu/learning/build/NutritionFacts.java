1a13df33d882665b90730e4e605a3fd787df050f
章小雨 <xiaoyu3@sfmail.sf-express.com>
2020-10-10 15:34:42 +0800

    添加构造器模式

---
+++ java/src/main/java/cn/xiaoyu/learning/build/NutritionFacts.java
@@ -0,0 +1,88 @@
+package cn.xiaoyu.learning.build;
+
+/**
+ * 构造方法参数过多时，可以采用builder模式来完成构造
+ *
+ * @author 01399268
+ * @date 2020/10/10
+ */
+public class NutritionFacts {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public static class Builder {
+        // Required parameters
+        private final int servingSize;
+        private final int servings;
+        // Optional parameters - initialized to default values
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbohydrate = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder calories(int val) {
+            calories = val;
+            return this;
+        }
+
+        public Builder fat(int val) {
+            fat = val;
+            return this;
+        }
+
+        public Builder sodium(int val) {
+            sodium = val;
+            return this;
+        }
+
+        public Builder carbohydrate(int val) {
+            carbohydrate = val;
+            return this;
+        }
+
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+
+    private NutritionFacts(Builder builder) {
+        servingSize = builder.servingSize;
+        servings = builder.servings;
+        calories = builder.calories;
+        fat = builder.fat;
+        sodium = builder.sodium;
+        carbohydrate = builder.carbohydrate;
+    }
+
+    @Override
+    public String toString() {
+        return "NutritionFacts{" +
+                "servingSize=" + servingSize +
+                ", servings=" + servings +
+                ", calories=" + calories +
+                ", fat=" + fat +
+                ", sodium=" + sodium +
+                ", carbohydrate=" + carbohydrate +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        NutritionFacts nutritionFacts = new NutritionFacts.Builder(5, 10)
+                .calories(1)
+                .carbohydrate(2)
+                .fat(3)
+                .sodium(4)
+                .build();
+
+        System.out.println(nutritionFacts);
+    }
+}
