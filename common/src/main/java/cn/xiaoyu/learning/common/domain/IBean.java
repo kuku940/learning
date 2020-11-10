8a70b24bb4be00b410b1742566ba69980b615d19
章小雨 <xiaoyu3@sfmail.sf-express.com>
2020-11-10 11:28:43 +0800

    添加一个通用的实体继承类

---
+++ common/src/main/java/cn/xiaoyu/learning/common/domain/IBean.java
@@ -0,0 +1,52 @@
+package cn.xiaoyu.learning.common.domain;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class IBean implements Serializable {
+    protected Long id;
+    protected String createUser;
+    protected Timestamp createTime;
+    protected String lastUpdateUser;
+    protected Timestamp lastUpdateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getLastUpdateUser() {
+        return lastUpdateUser;
+    }
+
+    public void setLastUpdateUser(String lastUpdateUser) {
+        this.lastUpdateUser = lastUpdateUser;
+    }
+
+    public Timestamp getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(Timestamp lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+}
